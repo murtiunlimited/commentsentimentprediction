a9db988231222812338030a3d3f87d523a587ab9
Murtaza Hussain <murtaza32004@gmail.com>
2025-11-24 00:32:34 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'murtiunlimited',
+    author_email= 'murtaza32004@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
